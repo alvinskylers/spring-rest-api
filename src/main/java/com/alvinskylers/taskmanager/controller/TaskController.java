1e4186df4a51d80e9092d73434022b04d53b3be5
alvinskylers <alvinskylers.dev@gmail.com>
2026-04-20 16:08:01 +0800

    feat: implement create and get tasks

--- src/main/java/com/alvinskylers/taskmanager/controller/TaskController.java
+++ src/main/java/com/alvinskylers/taskmanager/controller/TaskController.java
@@ -1,13 +1,31 @@
 package com.alvinskylers.taskmanager.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.alvinskylers.taskmanager.entity.Task;
+import org.springframework.web.bind.annotation.*;
 
-@RestController
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController // @Controller and @ResponseBody
+@RequestMapping("/api/v1/tasks") // Base URL for all endpoints in this controller
 public class TaskController {
 
-    @GetMapping("/hello")
-    public String sayHello() {
-        return "Hello there!";
+    List<Task> tasks = new ArrayList<>();
+    private long taskId = 1L;
+
+    @GetMapping
+    public List<Task> getAllTasks() {
+        return tasks;
     }
+
+    @PostMapping
+    public void createTask(@RequestBody Task task) {
+        task.setId(taskId++);
+        task.setCompleted(false);
+        task.setCreatedAt(LocalDateTime.now());
+        tasks.add(task);
+    }
+
+
 }
