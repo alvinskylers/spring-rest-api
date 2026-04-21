54ac00953e8715898dc3454a6e248bad00017b7e
alvinskylers <alvinskylers.dev@gmail.com>
2026-04-21 17:53:46 +0800

    feat: implement custom query methods in task repository

--- src/main/java/com/alvinskylers/taskmanager/repository/TaskRepository.java
+++ src/main/java/com/alvinskylers/taskmanager/repository/TaskRepository.java
@@ -2,6 +2,19 @@ package com.alvinskylers.taskmanager.repository;
 
 import com.alvinskylers.taskmanager.entity.Task;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
+import java.util.List;
+
+// @Repository isn't always necessary because JpaRepository already uses it
 public interface TaskRepository extends JpaRepository<Task, Long> {
+
+    //Custom Methods
+     List<Task> findByCompleted(boolean completed);
+    List<Task> findByTitleContaining(String query);
+
+    //Custom Query
+    @Query("SELECT t FROM Task t where t.completed = :completed")
+    List<Task> findTasksByCompletionStatus(@Param("completed") boolean completed);
 }
