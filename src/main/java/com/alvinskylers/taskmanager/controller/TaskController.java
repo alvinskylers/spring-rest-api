3466fd05c7aa9cfd4ea13018d0846fe1fc522ddb
alvinskylers <alvinskylers.dev@gmail.com>
2026-04-29 21:38:50 +0800

    add task exception class refactor: controller and service class for task entity

--- src/main/java/com/alvinskylers/taskmanager/controller/TaskController.java
+++ src/main/java/com/alvinskylers/taskmanager/controller/TaskController.java
@@ -26,10 +26,8 @@ public class TaskController {
     }
 
     @GetMapping("/{id}")
-    public ResponseEntity<Task> getTaskById(@PathVariable Long id) {
-        return taskService.getTaskById(id)
-                .map(ResponseEntity::ok)
-                .orElse(ResponseEntity.notFound().build());
+    public Task getTaskById(@PathVariable Long id) {
+        return taskService.getTaskById(id);
     }
 
     @PostMapping
@@ -39,18 +37,15 @@ public class TaskController {
     }
 
     @PutMapping("/{id}")
-    public ResponseEntity<Task> updateTask(@PathVariable Long id,
+    public Task updateTask(@PathVariable Long id,
                                            @RequestBody Task updatedTask) {
-        return taskService.updateTask(id, updatedTask)
-                .map(ResponseEntity::ok)
-                .orElse(ResponseEntity.notFound().build());
+        return taskService.updateTask(id, updatedTask);
     }
 
     @DeleteMapping("/{id}")
     public ResponseEntity<Void> deleteTask(@PathVariable Long id) {
-        return taskService.deleteTask(id) ?
-                ResponseEntity.noContent().build() :
-                ResponseEntity.notFound().build();
+        taskService.deleteTask(id);
+        return ResponseEntity.ok().build();
     }
 
     @GetMapping("/completed/{status}")
