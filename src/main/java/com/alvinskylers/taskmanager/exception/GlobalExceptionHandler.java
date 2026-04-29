a66d3f79ff217ee5012e6d52dc17d31f07f0dcbd
alvinskylers <alvinskylers.dev@gmail.com>
2026-04-29 22:09:12 +0800

    add: GlobalExecptionHandler class

---
+++ src/main/java/com/alvinskylers/taskmanager/exception/GlobalExceptionHandler.java
@@ -0,0 +1,9 @@
+package com.alvinskylers.taskmanager.exception;
+
+import org.springframework.web.bind.annotation.ControllerAdvice;
+
+@ControllerAdvice
+public class GlobalExceptionHandler {
+
+
+}
