6cd05e9e181e9437443dea043af8aec5a690644f
alvinskylers <alvinskylers.dev@gmail.com>
2026-05-05 16:14:52 +0800

    add: pagination methods in task repository

--- src/main/java/com/alvinskylers/taskmanager/repository/TaskRepository.java
+++ src/main/java/com/alvinskylers/taskmanager/repository/TaskRepository.java
@@ -1,6 +1,8 @@
 package com.alvinskylers.taskmanager.repository;
 
 import com.alvinskylers.taskmanager.entity.Task;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
@@ -12,9 +14,16 @@ public interface TaskRepository extends JpaRepository<Task, Long> {
 
     //Custom Methods
      List<Task> findByCompleted(boolean completed);
-    List<Task> findByTitleContaining(String query);
+     List<Task> findByTitleContaining(String query);
 
     //Custom Query
     @Query("SELECT t FROM Task t where t.completed = :completed")
     List<Task> findTasksByCompletionStatus(@Param("completed") boolean completed);
+
+    //Pagination Implementations
+    Page<Task> findByCompleted(boolean completed, Pageable pageable);
+    Page<Task> findByTitleContaining(String query, Pageable pageable);
+    @Query("SELECT t FROM Task t where t.completed = :completed")
+    Page<Task> findTasksByCompletionStatus(@Param("completed") boolean completed, Pageable pageable);
+
 }
