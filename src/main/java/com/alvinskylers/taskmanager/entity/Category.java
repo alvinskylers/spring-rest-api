dbe0d1c29fa1349a2fbfc72451e31ebd65e983bf
alvinskylers <alvinskylers.dev@gmail.com>
2026-05-07 23:27:20 +0800

    add: category entity

---
+++ src/main/java/com/alvinskylers/taskmanager/entity/Category.java
@@ -0,0 +1,28 @@
+package com.alvinskylers.taskmanager.entity;
+
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "categories")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class Category {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(length = 255)
+    private String description;
+
+}
